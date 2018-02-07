d61f59d9a62a0aa484b7e835d7a5e6ca30f03ffa
Simon Redding <s1m0n.r3dd1ng@gmail.com>
2018-02-07 15:59:31 +0000

    basic skeleton

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='logbucket',
+      version='0.1',
+      description='Event logging with MQTT and MongoDB',
+      url='http://github.com/oldironhorse/logbucket',
+      license='GPL 3.0',
+      packages=['logbucket'],
+      install_requires=[
+        'pymongo',
+        'paho-mqtt',
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
